1f0625d48d1f91a3f7150767128dd58c390d3c7e
Md <mdmonis128@gmail.com>
2024-07-30 12:47:54 +0530

    Differentiate Between Array and Objects

--- 2.js
+++ 2.js
@@ -1,6 +1,7 @@
 // How to check if array or object
-function isArray(elem){
-    return Array.isArray(elem);
-}
-console.log(isArray([1,2,3]));
-console.log(isArray({}));
+let m = [1, 2, 3, 4];
+let d = { 1: 123, 2: 234 };
+console.log(typeof(m));
+console.log(typeof(d));
+console.log(Array.isArray(m));
+console.log(Array.isArray(d));
