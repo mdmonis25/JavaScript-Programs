08ddab18bf901b9fb685c442c5a96f9e14f0d017
Md <mdmonis128@gmail.com>
2024-07-30 13:51:22 +0530

    Sum of all numbers in Array

--- 11.js
+++ 11.js
@@ -1,10 +1,8 @@
 // add all elements of the array
 
-function sum(arr){
-    var sum =0;
-    arr.forEach(element => {
-     sum += element;   
-    });
-    return sum;
-}
-console.log(sum([1,2,3,4,5]))
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+a.forEach((elem) => {
+  sum += elem;
+});
+console.log(sum);
