b7298813f3a9e607d8253c448d1e18878f7083b8
Md <mdmonis128@gmail.com>
2024-07-30 13:25:25 +0530

    Sorting a String in Alphabetical Order

--- 8.js
+++ 8.js
@@ -1,6 +1,4 @@
 // convert string to alphabetical order 
- 
-strSort = (str) => {
-    return str.split("").sort().join("");
-}
-console.log(strSort("monis"))
+
+let a = "monis";
+console.log(a.split("").sort().join(""));
